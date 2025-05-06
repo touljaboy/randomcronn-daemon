f7e57d0ee4e0894e98b2e0572d9e6bd7b4349f1a
touljaboy <ernest.fudali@gmail.com>
2025-05-06 11:51:42 +0000

    test daemon code

---
+++ randomcronn.c
@@ -0,0 +1,247 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <time.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+// Ile maksymalnie zadan - potrzebne przy definicji tabeli z zadaniami
+#define MAX_TASKS 100
+// Ile maksymalnie znakow ma jedna linia polecenia - do przechowywania stringa z poleceniem w strukturze Task
+#define MAX_LINE 256
+
+// Struktura do przechowywania pojedynczego zadania, przechowuje komende, tryb (0, 1, 2) oraz czas odpalenia.
+typedef struct {
+    char command[MAX_LINE];
+    int mode;
+    time_t run_at;
+} Task;
+
+// Tabela przechowujaca zadania do wykonania
+Task tasks[MAX_TASKS];
+// licznik zadan
+int task_count = 0;
+// sciezka do pliku z outputem
+char *outfile_path;
+// sciezka do pliku z zadaniami
+char *taskfile_path;
+// flaga uzywana do przerwania dzialaniu daemona, potem uzywana w funkcji main w loopie while(running)
+int running = 1;
+
+// obsluga sigint przy wyslaniu sygnalu do daemona (kill -SIGINT [...])
+void handle_sigint(int sig) {
+    // zapisanie do logow systemowych
+    syslog(LOG_INFO, "Received SIGINT. Exiting after current task.");
+    running = 0;
+}
+
+// przeladowanie pliku z zadaniamis
+void handle_sigusr1(int sig) {
+    syslog(LOG_INFO, "Received SIGUSR1. Reloading tasks.");
+    task_count = 0;
+}
+
+// wypisanie pozostalych zadan do logow systemowych
+void handle_sigusr2(int sig) {
+    syslog(LOG_INFO, "Received SIGUSR2. Remaining tasks:");
+    for (int i = 0; i < task_count; i++) {
+        if (tasks[i].run_at != 0) {
+            syslog(LOG_INFO, "Task: %s", tasks[i].command);
+        }
+    }
+}
+
+// funkcja przetasowania zadan w tabeli, aby zgodnie z trescia wykonac je w losowej kolejnosci
+void shuffle_tasks(Task *tasks) {
+    for(int i = task_count - 1; i > 0; i--) {
+        int j = rand() % (i-1);
+        // zamieniamy
+        Task temp = tasks[i];
+        tasks[i] = tasks[j];
+        tasks[j] = temp;
+    }
+}
+
+// ladowanie zadan z pliku taskfile
+void load_tasks() {
+    FILE *f = fopen(taskfile_path, "r");
+    if (!f) {
+        syslog(LOG_ERR, "Failed to open task file");
+        exit(EXIT_FAILURE);
+    }
+
+    char line[MAX_LINE];
+    int hour_start, min_start, hour_end, min_end;
+    fgets(line, sizeof(line), f);
+    sscanf(line, "%d:%d;%d:%d", &hour_start, &min_start, &hour_end, &min_end);
+
+    // zapisujemy obecny czas w zmiennej
+    time_t now = time(NULL);
+    // struct tm - zdefiniowany w time.h, start_tm zmienna ktora przechowuje czas
+    struct tm start_tm = *localtime(&now);
+    // uzupelniamy structa danymi godziny i minuty zdefiniowanej w taskfile jako pierwsza
+    start_tm.tm_hour = hour_start;
+    start_tm.tm_min = min_start;
+    start_tm.tm_sec = 0;
+    // konwertujemy na timestamp, aby latwo porownywac czas
+    time_t start_time = mktime(&start_tm);
+
+
+    // to samo co wyzej tylko do czasu zakonczenia zdefiniowanego w taskfile
+    struct tm end_tm = *localtime(&now);
+    end_tm.tm_hour = hour_end;
+    end_tm.tm_min = min_end;
+    end_tm.tm_sec = 0;
+    time_t end_time = mktime(&end_tm);
+
+    task_count = 0;
+    // wczytujemy kolejne linie z pliku, ktore sa komendami. Jednoczesnie sprawdzamy czy nie przekraczamy maksymalnej liczby zadan (100)
+    while (fgets(line, sizeof(line), f) && task_count < MAX_TASKS) {
+        // rozdzielamy linie, separatorem jest dwukropek lub nowa linia. Pierwszy to komenda, drugi to tryb
+        char *cmd = strtok(line, ":\n");
+        char *mode_str = strtok(NULL, ":\n");
+        // sprawdzamy poprawnosc zdefiniowanej linii
+        if (cmd && mode_str) {
+            // kopiujemy polecenie do struktury task o indeksie task_count.
+            strncpy(tasks[task_count].command, cmd, MAX_LINE);
+            // za pomoca atoi, konwertujemy stringa do inta i nadajemy tryb
+            tasks[task_count].mode = atoi(mode_str);
+            tasks[task_count].run_at = 0; // tymczasowo ustawiamy na 0, w dalszej czesci ustawiamy czas startu aby dodac losowosc do zadan
+            task_count++;
+        }
+    }
+    fclose(f);
+
+        // czas miedzy zadaniami to 10 sekund, wiec task_count * 10 tworzy nam ten odstep
+        // odejmujemy zdefiniowany koniec czasu zadania od jego poczatku oraz potrzebny czas miedzy zadaniami
+
+        // moze dojsc do sytuacji gdzie mamy zbyt wiele zadan na zbyt male okienko czasowe (bo uwzgledniamy przerwy) 
+        // wowczas przerywamy dzialanie daemona
+        int range = difftime(end_time, start_time) - task_count * 10;
+        if (range < 0) {
+            syslog(LOG_ERR, "Not enough time to schedule all of the defined tasks with a 10s window between each one. ");
+        }
+    
+    // losujemy kolejnosc zadan
+    shuffle_tasks(tasks);
+    // nadajemy losowy czas wykonania wczytanym zadaniom
+    srand(time(NULL));
+    for (int i = 0; i < task_count; i++) {
+        // losujemy tez czas uruchomienia zadania. w danym przedziale
+        tasks[i].run_at = start_time + (rand() % (range + 1)) + i * 10;
+    }
+}
+
+// wywolywanie pojedynczego zadania poprzez utworzenie potoku
+void execute_task(Task *task) {
+    // tworzymy pipe
+    int fds[2];
+    pipe(fds);
+    pid_t pid = fork();
+    // jesli pid to zero, znajdujemy sie w procesie potomnym, else proces rodzica
+    if (pid == 0) {
+        // zamykamy poczatek pipe'a od odbierania danych potomka, bo bedziemy tylko dane wysylac do fds[1]
+        close(fds[0]);
+        
+        // otwieramy plik outfile, write-only, append, tworzymy plik jesli nie istnieje pod dana sciezka, 0644 dostep rwrr
+        int out_fd = open(outfile_path, O_WRONLY | O_APPEND | O_CREAT, 0644);
+        // w zaleznosci od trybu, przekierowujemy odpowiednie outputy do pliku
+        if (task->mode == 0) {
+            // przekierowujemy stdout do pliku
+            dup2(out_fd, STDOUT_FILENO);
+        } else if (task->mode == 1) {
+            // przekierowujemy sterr do pliku
+            dup2(out_fd, STDERR_FILENO);
+        } else {
+            // przekierowujemy stdout i stderr do pliku
+            dup2(out_fd, STDOUT_FILENO);
+            dup2(out_fd, STDERR_FILENO);
+        }
+        // zapisujemy informacje o uruchomionej komendzie do outfile
+        dprintf(out_fd, "\n[Command: %s]\n", task->command);
+        // uruchamiamy bash'a z opcja -c gdzie przekazujemy argument w postaci komendy aktualnego task'a
+        execl("/bin/sh", "sh", "-c", task->command, NULL);
+        // w przypadku gdy np nie znajdziemy komendy, proces potomny opusci proces rodzica
+        exit(1);
+    } else {
+        // zamykamy koniec pipe'a od odbierania danych rodzica, tylko wysylac do fds[0]
+        close(fds[1]);
+        int status;
+        // oczekiwanie na zakonczenie wywolanego potomnego task'a
+        waitpid(pid, &status, 0);
+        syslog(LOG_INFO, "Executed: %s, Exit code: %d", task->command, WEXITSTATUS(status));
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    // w przypadku braku podania sciezki do taskfile oraz outfile, daemon nie wywola sie
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <taskfile> <outfile>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // przypisujemy sciezki do plikow z komendy do zmiennych
+    taskfile_path = argv[1];
+    outfile_path = argv[2];
+
+    // tylko proces potomny kontynuuje dzialanie programu - demonizacja
+    pid_t pid = fork();
+    if (pid < 0) exit(EXIT_FAILURE);
+    if (pid > 0) exit(EXIT_SUCCESS);
+
+
+    // demon moze tworzyc pliki z pelnymi uprawnieniami
+    umask(0);
+    // otwieramy polaczenie z syslogiem, ustawiamy nazwe, pid oraz typ jako daemon
+    openlog("minicron", LOG_PID, LOG_DAEMON);
+
+    // odlaczamy proces od dzialania terminala, zmieniamy working directory na '/'
+    pid_t sid = setsid();
+    if (sid < 0) exit(EXIT_FAILURE);
+    if ((chdir("/")) < 0) exit(EXIT_FAILURE);
+
+    // zamykamy standardowe deskrypory, blokujac komunikacje daemona z terminalem
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    // definicje sygnalow z tresci zadania, podpinamy je pod wczesniej zdefiniowane voidy
+    signal(SIGINT, handle_sigint);
+    signal(SIGUSR1, handle_sigusr1);
+    signal(SIGUSR2, handle_sigusr2);
+
+    // wczytujemy taski z pliku
+    load_tasks();
+
+
+    while (running) {
+        // pobieramy aktualny czas
+        time_t now = time(NULL);
+        // daemon bedzie dzialal (lub spal) przez 15min, dajac uzytkownikowi szanse na np. przeladowanie taskfile
+        time_t next = now + 900;
+        for (int i = 0; i < task_count; i++) {
+            // nadszedl czas wykonania zadania i nie jest oznaczone jako wykonane (run_at = 0)
+            if (tasks[i].run_at <= now && tasks[i].run_at != 0) {
+                execute_task(&tasks[i]);
+                tasks[i].run_at = 0;
+            }
+            // wykonujemy zadanie priorytetowo, wiec aktualizujemy next aby odpowiednio wybudzic daemona do jego wykonania
+            else if (tasks[i].run_at > now && tasks[i].run_at < next) {
+                next = tasks[i].run_at;
+            }
+        }
+        // daemon zasypia na czas do kolejnego zadania (else if wyzej), albo na zdefiniowany czas 15min
+        sleep(next - now);
+    }
+
+    syslog(LOG_INFO, "Demon exiting.");
+    closelog();
+    return 0;
+}
